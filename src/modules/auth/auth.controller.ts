ff49bb443b8eb1e9fb5a1d6c8339cdde32ddbd95
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-06-29 20:55:41 +0600

    implemented refresh-token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -31,10 +31,27 @@ const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunct
     })
 })
 
+const refreshToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken
 
+    const { accessToken } = await authService.refreshToken(refreshToken)
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24 // 24 hour or 1 day
+    })
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Refresh Token Successfully",
+        data: { accessToken }
+    })
 
+})
 
 
 export const authController = {
-    loginUser
+    loginUser,
+    refreshToken
 }
